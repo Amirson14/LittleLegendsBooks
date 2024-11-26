ee80c4a14f9943f34655c6f304ec14fe916bca11
Amir Greenberg <amir.greenberg@t-a9.com>
2024-11-26 11:27:52 +0200

    Initial commit with Firebase Hosting setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsmoYNd9w_nVNhJQSsE86-9nChZCrhnsc",
+  authDomain: "littlelegendsbooks-d07f6.firebaseapp.com",
+  projectId: "littlelegendsbooks-d07f6",
+  storageBucket: "littlelegendsbooks-d07f6.firebasestorage.app",
+  messagingSenderId: "498624005213",
+  appId: "1:498624005213:web:c4bb319b042c4593aff9b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
