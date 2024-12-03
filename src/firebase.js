64ce7291c6d517b15f8a6d6b05492a46a029ba88
Amir Greenberg <amir.greenberg@t-a9.com>
2024-12-03 09:40:24 +0200

    add analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,10 @@ const firebaseConfig = {
   projectId: "littlelegendsbooks-d07f6",
   storageBucket: "littlelegendsbooks-d07f6.firebasestorage.app",
   messagingSenderId: "498624005213",
-  appId: "1:498624005213:web:c4bb319b042c4593aff9b4"
+  appId: "1:498624005213:web:c4bb319b042c4593aff9b4",
+  measurementId: "G-MN2FC77Q1F"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
