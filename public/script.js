5c2f4f8786f7208d505d7cd33a7d5f9d23afb3d5
Amir Greenberg <amir.greenberg@t-a9.com>
2024-12-09 16:50:01 +0200

    banner

--- public/script.js
+++ public/script.js
@@ -1,2 +1,15 @@
 // Update copyright year
 document.getElementById('current-year').textContent = new Date().getFullYear();
+
+function openLightbox(image) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    
+    lightboxImg.src = image.src; // Set the source of the lightbox image
+    lightbox.style.display = 'flex'; // Display the lightbox
+  }
+  
+  function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none'; // Hide the lightbox
+  }
